44d91530733cae769a8dd74f086faf2c5170d91f
nikitanicvetov <nikita.nitsvetov@gmail.com>
2025-09-01 13:55:09 +0300

    multi checbox

--- pages/test_select_multiple_select.py
+++ pages/test_select_multiple_select.py
@@ -33,6 +33,7 @@ def test_dropdow_select1(browser,data):
     multiple_select_page.dropdown_select_value1(data)
     multiple_select_page.should_have_selected_value1(data)
 
+@allure.feature('Verify second Dropdown')
 @pytest.mark.parametrize('data',data()[:4])
 def test_dropdow_select2(browser,data):
     multiple_select_page = MultipleSelect(browser)
@@ -41,6 +42,7 @@ def test_dropdow_select2(browser,data):
     multiple_select_page.dropdown_select_value2(data)
     multiple_select_page.should_have_selected_value2(data)
 
+@allure.feature('Verify third Dropdown')
 @pytest.mark.parametrize('data',data()[:3])
 def test_dropdow_select3(browser,data):
     multiple_select_page = MultipleSelect(browser)
@@ -49,6 +51,7 @@ def test_dropdow_select3(browser,data):
     multiple_select_page.dropdown_select_value3(data)
     multiple_select_page.should_have_selected_value3(data)
 
+@allure.feature('VWork Dropdown')
 def test_dropdown_multi_select(browser):
     multiple_select_page = MultipleSelect(browser)
     multiple_select_page.open()
@@ -70,6 +73,7 @@ def request_mesages():
         'After submitting the form, the option selected by the user should be placed into the resulting phrase and displayed to the user',
         'Resulting phrase template: "to go by <transport> to the <destination> <when>"'
     ]
+@allure.feature('Requirement')
 @pytest.mark.parametrize('request_mesages', request_mesages())
 def test_requirement(browser, request_mesages):
     multiple_select_page = MultipleSelect(browser)
